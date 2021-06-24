a7b2dab44aed41c2f0d0b619c5f8dafab27c0f08
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2021-06-24 09:26:23 -0400

    Update ps0.py

--- ps0/ps0.py
+++ ps0/ps0.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-x = input("Enter a number 'x': ")
-y = input("Enter a number 'y': ")
+x = int(input("Enter a number 'x': "))
+y = int(input("Enter a number 'y': "))
 print("x**y = {}".format(x**y))
-print("log(x) = {}".format(np.log2(x))
+print("log(x) = {}".format(np.log2(x)))
 
 
 
