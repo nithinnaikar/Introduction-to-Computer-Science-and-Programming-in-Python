29d79d604f9ed1cb5f107af17af16a0c672e3e49
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2021-12-20 18:42:42 -0500

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 18:35:35 2021
+
+@author: Nithin
+"""
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y = {}".format(x**y))
+print("log(x) = {}".format(numpy.log2(x)))
